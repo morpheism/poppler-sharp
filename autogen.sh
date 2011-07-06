ffb262ea99e1978832f2fc5e5e82540aed3504bb
Chow Loong Jin <hyperair@ubuntu.com>
2011-07-06 20:56:01 +0800

    Allow out-of-tree builds for autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -1,6 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 
+dir="$(dirname "$0")"
+pushd "$dir"
 aclocal -I /usr/share/aclocal -I m4
 automake -ac
 autoconf
-[ -z "$NOCONFIGURE" ] && ./configure "$@"
+popd
+[ -z "$NOCONFIGURE" ] && "$dir/configure" "$@"
